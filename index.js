6f395e49a2846a010828f5446bcce352950527ce
Winner Luyeye <winnerluyeye1@gmail.com>
2024-03-07 21:07:13 +0100

    Retrieving and displaying articles by id

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ server.get("/articles", (req, res) => {
     res.send({articles});
 })
 
+server.get("/articles/:id", (req, res) => {
+    const id = req.params.id;
+    const foundArticle = articles.find(articles => articles.id == id)
+    console.log(``Articles ${id});
+    if (foundArticle) {
+        res.statut(200).json(foundArticle);
+    } else {
+        res.statut(404).send("Article Not Found")
+    }
+})
